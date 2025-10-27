5c20622b628f72ed20393d33814d1e9d5ee98c78
abosotaif <110945922+abosotaif@users.noreply.github.com>
2025-10-28 02:03:48 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  
+  base: '/Hi/', 
+})
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
